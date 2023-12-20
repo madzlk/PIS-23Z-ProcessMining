e2242a6537b1812bfe5b1f005f7f9d74bfefc466
Magdalena <magdudinf@gmail.com>
2023-12-20 14:59:24 +0100

    added pytest file

---
+++ backend/test_log.py
@@ -0,0 +1,12 @@
+import pytest
+from .log import Log
+
+def test_sanity():
+    assert True
+
+def test_log_creation():
+    log = Log(1,"IM00001", "Reasignment", '07-01-2013 08:17:54')
+    assert log.log_id == 1
+    assert log.incident_id == 'IM00001'
+    assert log.date_stamp == '07-01-2013 08:17:54'
+    assert log.incident_activity_type == "Reasignment"
